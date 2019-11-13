e24c9aaf94aeee884c4eb3e39a603b2a4d5420c5
Jez Bayliss <jezbayliss@hotmail.com>
2019-11-13 00:28:39 +0000

    Lots of goodness VV
    1) Build what George wrote earlier but didn't push with POST request of message data in the new es6 syntax
    2) Can press enter as well as click send
    3) Simple back and forth between client and backend 'reasoning controller'

--- flaskChatbot/static/js/script.js
+++ flaskChatbot/static/js/script.js
@@ -1,40 +1,67 @@
-
-
 function updateScroll(){
-
     var objDiv = document.getElementById("textfield");
     objDiv.scrollTop = objDiv.scrollHeight;
 }
 
+
+
+sendMessage = () => {
+
+    // Get last message sent
+    var lastMessage = $(".response:last-of-type")
+
+    // Get user input
+    var userMessage = document.getElementById('userInput').value
+    console.log("PaPa's new friend: "+userMessage)
+    document.getElementById('userInput').value = "" //Empy input
+
+    // Write user message bubble
+    lastMessage.after(
+        "<div class='response message-right '>  <p class='bg-success'> "+userMessage+" </p>  </div>"
+    );
+
+    setTimeout(()=>{  //REMOVE TIMEOUT WHEN WE HAVE ACTUAL PROCESSING xD
+
+        //Fetch request
+        fetch('/chat',{
+            method: 'POST',
+            body: JSON.stringify({ userMessage: userMessage }), // Provide user query message
+            headers: {
+            'Content-Type': 'application/json',
+            }
+        }).then((response) => {
+            return response.json(); //Parse as JSON
+        }).then((data) => {
+            console.log("PaPa: "+data.answer)
+            // Write PaPa message bubble
+            lastMessage.after(
+                "<div class='response message-left '>  <p class='bg-primary'> "+data.answer+" </p>  </div>"
+            );
+        }).catch(() => {
+            console.log("Could not recieve response from PaPa :(");
+        });
+
+    }, 1000);
+
+    updateScroll();
+    
+}
+
 // JQUERY --------------------------------------------------
 
-$(document).ready(function(){
+$(document).ready(()=>{
 
-    
+    document.getElementById('userInput').onkeypress = (e) => {
+        if (!e) e = window.event;
+        var keyCode = e.keyCode || e.which;
+        if (keyCode == '13'){
+          // Enter pressed
+          sendMessage()
+        }
+      }
 
     // Sends alternating messages on clicking 'send' /chatbot.html
-    $(".send-btn").click(function(){
-
-        // Get last message sent
-        var lastMessage = $(".response:last-of-type")
-
-        // Check if it was right or left side and respond
-        if(lastMessage.hasClass("message-right")){
-            lastMessage.after(
-                "<div class='response message-left '>  <p class='bg-primary'> Helping... </p>  </div>"
-            );
-        }
-        else {
-            lastMessage.after(
-                "<div class='response message-right '>  <p class='bg-success'> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip... </p>  </div>"
-            );
-        }
-
-        updateScroll();
-    });
-  
-  });
-
-
+    $(".send-btn").click(()=>sendMessage())
+})
 
 // JQUERY END --------------------------------------------------
