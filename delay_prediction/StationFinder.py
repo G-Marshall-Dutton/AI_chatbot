4321010c00064ace0b420a47d22a0686ecc2c4b7
Matt <ecstatic@gmail.com>
2020-01-08 20:11:51 +0000

    Added getStation()
    Also, refactored so dictionaries are only loaded once

--- delay_prediction/StationFinder.py
+++ delay_prediction/StationFinder.py
@@ -7,22 +7,35 @@ def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 class StationFinder:
+    # class attributes - empty dictionaries
+    stationToCodeDict = dict()
+    codeToStationDict = dict()
 
-    # returns code
-    def getCode(self, station):
+    # constructor, load the dictionaries once
+    def __init__(self):
         # set filepath
         filepath = Path(__file__).parent / "../delay_prediction/reference_data/stations_processed.csv"
         
-        # open file into csv
+        # create station to code dictionary
         with open(filepath) as f:
-            d = dict(filter(None, csv.reader(f)))
+            self.stationToCodeDict = dict(filter(None, csv.reader(f)))
+
+        # create code to station dictionary
+        with open(filepath) as f:
+             d = dict(filter(None, csv.reader(f)))
+
+             # create dictionary in csv order, then swap keys and values
+             self.codeToStationDict = dict((v,k) for k,v in d.items())
+
+    # returns code
+    def getCode(self, station):
 
         # set up comparison stuff
         best_key_score = 0
         best_key = None
 
         # iterate through dictionary
-        for key in d:
+        for key in self.stationToCodeDict:
             # calculate similar score
             curr_score = similar(station, key)
             
@@ -31,7 +44,26 @@ class StationFinder:
                 best_key_score = curr_score
                 best_key = key
 
-        return d[best_key]
+        return self.stationToCodeDict[best_key]
+
+    # returns station
+    def getStation(self, code):
+
+        # set up comparison stuff
+        best_key_score = 0
+        best_key = None
+
+        # iterate through dictionary
+        for key in self.codeToStationDict:
+            # calculate similar score
+            curr_score = similar(code, key)
+            
+            # if better, keep track of it
+            if curr_score > best_key_score:
+                best_key_score = curr_score
+                best_key = key
+
+        return self.codeToStationDict[best_key]
 
 # TEST CODE
 sf = StationFinder()
@@ -40,7 +72,12 @@ while(True):
     # get user input
     userInput = input("Input for Station Finder > ")
 
-    print(sf.getCode(userInput))
+    # TEST FOR GET CODE
+    #print(sf.getCode(userInput))
+
+    # TEST FOR GET STATION
+    print(sf.getStation(userInput))
+
 
 
 
